a96c60ec30eaf2be2e166762ba5e03dd77c631eb
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-03-05 21:52:49 -0500

    leave comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import Navbar from '../Navbar';
 import React from 'react';
+// import react to gain access to its packages
 
 function App() {
+  // returns JSX
+  // renders Navbar component
   return (
     <div className="App">
         <Navbar />
