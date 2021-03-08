c8fcc0a077d4c0007e0e849a1d2339c3bda69bef
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-03-07 20:45:08 -0500

    leaving notes in syntax

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
       <>
+      {/* short syntax for Fragment. allows you to return multiple elements and group a list of children without adding extra nodes to the DOM */}
         <nav className="navbar">
+          {/* nav defines a set of navigation links */}
             <div className=".navbar-container">
                 <Link to="/" className="navbar-logo">
                   DCT <i class="fab fa-wolf-pack-battalion"></i>
