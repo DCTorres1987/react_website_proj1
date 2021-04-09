d9633ecd303ae186e2f3261a5fd5bd95c8022bd8
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-08 23:32:40 -0400

    add comments to stateless component

---
+++ src/components/HeroSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import react library
+
+function HeroSection() {
+    // stateless function
+
+    return (
+        // JSX returning a video on loop, h1 tag, and paragraph tag
+        <div className='hero-container'>
+            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+        </div>
+    )
+}
+
+export default HeroSection;
+
