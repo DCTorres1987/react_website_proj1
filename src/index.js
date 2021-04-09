6c945ae8b14c03b5140d70feb3c7de039c3078f6
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-08 23:35:53 -0400

    adding comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// importing react library
+// importing reactdom to gain access to virtual DOM
+// import in app component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// render app component
 ReactDOM.render(
     <App />,
   document.getElementById('root')
