7b9bdd7fcd10ba53af888e0d721a8bc3900e0654
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-09 23:12:17 -0400

    work on herosection css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from './Button';
+import './HeroSection.css';
 // import react library
 
 function HeroSection() {
@@ -11,8 +12,14 @@ function HeroSection() {
             <video src="/videos/video-2.mp4" autoPlay loop muted />
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
-            <Button className="btns" buttonStyle="btn-outline"
-            buttonSize="btn-large"></Button>
+            <div className="hero-btns">
+                <Button 
+                    className="btns" 
+                    buttonStyle="btn--outline"
+                    buttonSize="btn--large">
+                        GET STARTED
+                </Button>
+            </div>
         </div>
     )
 }
