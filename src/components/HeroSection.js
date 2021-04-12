2d5d4b5051320c3d9227ead68e8a98877362b648
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-11 22:51:54 -0400

    render home component. having trouble getting video to display

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,7 +10,7 @@ function HeroSection() {
     return (
         // JSX returning a video on loop, h1 tag, and paragraph tag
         <div className='hero-container'>
-            <video src="/videos/video-2.mp4" autoPlay loop muted />
+            <video src='/videos/video-2.mp4' autoPlay loop muted />
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import '../../App.css'
 import HeroSection from '../HeroSection'
 
-function Home () {
+function Home() {
 
     return (
         <>
