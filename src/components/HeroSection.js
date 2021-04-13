de377084b69ee3b246067ecf686adfb6bda3379d
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-04-12 22:41:40 -0400

    work on troubleshooting issue with mp4 file

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,8 +9,8 @@ function HeroSection() {
 
     return (
         // JSX returning a video on loop, h1 tag, and paragraph tag
-        <div className='hero-container'>
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
+        <div className="hero-container">
+            <video src="./videos/video-2.mp4" autoPlay loop muted />
             <h1>ADVENTURE AWAITS</h1>
             <p>What are you waiting for?</p>
             <div className="hero-btns">
@@ -25,7 +25,7 @@ function HeroSection() {
                     buttonStyle="btn--primary"
                     buttonSize="btn--large">
                         WATCH TRAILER 
-                        <i className='far fa-play-circle' />
+                        <i className="far fa-play-circle" />
                 </Button>
             </div>
         </div>
